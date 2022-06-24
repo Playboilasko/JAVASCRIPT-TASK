a1a4fb6c7511fd73104d823662f0abfc2594db30
Fawaz_F <falanafawaz.a@gmail.com>
2022-06-24 13:11:43 -0700

    JS-TASK
    javascript-task

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Javascript-Task
+Javascript code that prints out your name, height, and country on the screen
---
+++ script.js
@@ -0,0 +1,5 @@
+const myName = "FALANA FAWAZ";
+const myHeight = "177cm";
+const myCountry = "NIGERIA";
+
+alert("My Name is " + myName+"\nMy Height is " + myHeight+"\nMy Country is " + myCountry)
